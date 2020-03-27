e3013a5c3a1a388d69fc4262fb5855df02767477
pavel <pavel.dudkov@icloud.com>
2020-03-27 14:15:51 +0400

    Update game

--- js/game.js
+++ js/game.js
@@ -1,59 +1,137 @@
-const modalWindow = document.querySelector('.modal-window');
-const main = document.querySelector('.main');
-let counterHits = 0;
-let counterMiss = 0;
+/**
+ * Глобальные переменные
+ */
+let btnBlock = true;
+let counterError = 0;
 let limit = 10;
-let currentPosition = 0;
-let currentStep = 0;
-const level = {
-    "easy": 1200,
-    "medium": 800,
-    "hard": 500
-};
+let numberSquare = 0;
+let startTime;
+let endTime;
 
+/**
+ * Расчет итогового времени
+ * @returns {*}
+ */
+function timing() {
+    return Math.fround((endTime - startTime) / 1000).toFixed(2);
+}
+
+
+/**
+ * Формирует случайное число
+ * @returns {number}
+ */
+function randomNumber() {
+    return Math.floor(Math.random() * 36) + 1;
+}
+
+
+/**
+ * Попадание в активный квадрат
+ */
+function hittingTarget(dataTarget) {
+    const number = dataTarget.innerHTML;
+    hideSquare(number);
+    limit--;
+    document.querySelector('.js-counter-square').textContent = `${limit}`;
+    round();
+}
+
+/***
+ * Попадание в не активный квадрат
+ */
+function errorTarget(dataTarget) {
+    if (btnBlock) {
+        return false;
+    }
+    dataTarget.style.backgroundColor = '#ff1100';
+    setTimeout(() => {
+        dataTarget.style.backgroundColor = '#ffa500';
+    }, 200);
+    counterError++;
+}
+
+/**
+ * Показывает активный квадрат
+ * @param number
+ */
 function showSquare(number) {
     let parent = document.querySelector(`.square-${number}`);
-    parent.classList.add('square-active');
-    parent.firstElementChild.classList.add('squaer__text-active');
+    parent.classList.remove('js-target-error');
+    parent.firstElementChild.classList.add('js-target-for-user');
 }
 
+/**
+ * Скрывает активный квадрат
+ * @param number
+ */
 function hideSquare(number) {
     let parent = document.querySelector(`.square-${number}`);
-    parent.classList.remove('square-active');
-    parent.firstElementChild.classList.remove('squaer__text-active');
+    parent.classList.add('js-target-error');
+    parent.firstElementChild.classList.remove('js-target-for-user');
 }
 
+/**
+ * Шаг игры
+ */
 function round() {
-    let number = randomNumber();
-    showSquare(number);
-    let count = 3;
-    //TODO: Сделать обратный отсчёт
-    setTimeout(() => {
-        hideSquare(number);
-    }, `${800}`);
-}
-
-
-function startGame(dataTarget) {
-    closeModalWindow();
-    setTimeout(() => {
+    let randomNum = randomNumber();
+    if (numberSquare !== randomNum) {
+        numberSquare = randomNum;
+        if (limit !== 0) {
+            showSquare(numberSquare);
+        } else {
+            endGame();
+        }
+    } else {
         round();
-    }, 1000);
+    }
 }
 
-
-function getPosition(dataTarget) {
-    return parseInt(dataTarget.firstElementChild.innerHTML)
+/**
+ * Перезапуск игры
+ */
+function restartGame() {
+    limit = 10;
+    counterError = 0;
+    numberSquare = 0;
+    startTime = new Date().getTime();
+    endTime = new Date().getTime();
+    document.querySelector('.game-board').style.display = 'grid';
+    document.querySelector('.start-btn').style.display = 'block';
+    document.querySelector('.js-counter-square').textContent = `${limit}`;
+    document.querySelector('.game-counter').style.display = 'block';
+    const resultText = document.querySelector('.result-game__text');
+    resultText ? resultText.style.display = 'none' : false;
+    const targetForUser = document.querySelector('.js-target-for-user');
+    targetForUser ? targetForUser.classList.remove('js-target-for-user') : false;
+    btnBlock = true;
 }
 
-
-function closeModalWindow() {
-    modalWindow.style.display = 'none';
+/**
+ * Старт игры
+ */
+function startGame() {
+    if (!btnBlock) {
+        return false;
+    }
+    btnBlock = false;
+    startTime = new Date().getTime();
+    round();
 }
 
-
-function openModalWindow() {
-    modalWindow.style.display = 'flex';
+/**
+ * Конец игры
+ */
+function endGame() {
+    endTime = new Date().getTime();
+    const gameTitle = document.querySelector('.game-title');
+    document.querySelector('.game-board').style.display = 'none';
+    document.querySelector('.game-counter').style.display = 'none';
+    document.querySelector('.start-btn').style.display = 'none';
+    let resultTime = timing();
+    let displayResult = `<span class="result-game__text">Вы нашли 10 зеленных квадратов за ${resultTime} секунд. Промахов: ${counterError}</span>`;
+    gameTitle.insertAdjacentHTML('afterend', displayResult);
 }
 
 
@@ -62,9 +140,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     body.addEventListener('click', function (event) {
         const dataTarget = event.target;
-        dataTarget.classList.contains('js-choise-level') ? openModalWindow() : false;
         dataTarget.classList.contains('js-start-game') ? startGame(dataTarget) : false;
-        dataTarget.classList.contains('square') ? getPosition(dataTarget) : false;
-        dataTarget.classList.contains('js-icon-close') ? closeModalWindow() : false;
+        dataTarget.classList.contains('js-target-for-user') ? hittingTarget(dataTarget) : false;
+        dataTarget.classList.contains('js-target-error') ? errorTarget(dataTarget) : false;
+        dataTarget.classList.contains('js-restart-game') ? restartGame(dataTarget) : false;
     })
 });
--- js/utils.js
+++ js/utils.js
@@ -1,16 +1,16 @@
-function randomNumber() {
-    return Math.floor(Math.random() * 36) + 1;
+/**
+ * Создает площадку игры
+ */
+function initialPosition() {
+    const gameBoard = document.querySelector('.game-board');
+    for (let item = 1; item < 37; item++) {
+        let square = `<div class="square square-${item} js-target-error"><span class="squaer__text">${item}</span></div>`;
+        gameBoard.insertAdjacentHTML('beforeend', square);
+    }
 }
 
+
+
 document.addEventListener("DOMContentLoaded", () => {
-    const gameBoard = document.querySelector('.game-board');
-
     initialPosition();
-
-    function initialPosition() {
-        for (let item = 1; item < 37; item++) {
-            let square = `<div class="square square-${item}"><span class="squaer__text">${item}</span></div>`;
-            gameBoard.insertAdjacentHTML('beforeend', square);
-        }
-    }
 });
