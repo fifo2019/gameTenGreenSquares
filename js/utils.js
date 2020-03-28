6b73fdebd14e3c3c5bff814f7c858ae1e6761543
pavel <pavel.dudkov@icloud.com>
2020-03-28 12:16:19 +0400

    fix bags

--- js/utils.js
+++ js/utils.js
@@ -4,7 +4,6 @@
 function initialPosition() {
     const gameBoard = document.querySelector('.game-board');
     for (let item = 1; item < 37; item++) {
-        // let square = `<div class="square square-${item} js-target-error"><span class="squaer__text" id="${item}"></span></div>`;
         let square = `<div class="square js-target-error" id="square-${item}"></div>`;
         gameBoard.insertAdjacentHTML('beforeend', square);
     }
